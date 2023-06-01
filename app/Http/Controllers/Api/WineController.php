f72776fb03b0d9f813c58d27280000fb91b55af3
alessandroamormino <amormino.alessandro97@gmail.com>
2023-06-01 16:24:54 +0200

    feat: add show function api

--- app/Http/Controllers/Api/WineController.php
+++ app/Http/Controllers/Api/WineController.php
@@ -9,11 +9,28 @@ use Illuminate\Http\Request;
 class WineController extends Controller
 {
     public function index(){
-        $wines = Wine::all();
+        // $wines = Wine::all();
+        $wines = Wine::with('winery', 'vineyards')->get();
 
         return response()->json([
           'success' => true,
           'results' => $wines,
         ]);
+    }
+
+    public function show($id){
+      $wine = Wine::where('id', $id)->with('winery', 'vineyards')->first();
+
+      if($wine){
+        return response()->json([
+          'success' => true,
+          'wine' => $wine,
+        ]);
+      } else {
+        return response()->json([
+          'success' => false,
+          'error' => 'Il vino selezionato non esiste',
+        ]);
+      }
     }
 }
